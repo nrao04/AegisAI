a97d765180d19e49fc46a67d7edb011cb0deebcb
nrao <nrao200423@gmail.com>
2025-03-26 20:28:55 -0700

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# root endpoint for testing and health checks
 @app.get("/")
 
 def read_root():
     return {"message": "Welcome to AegisAI – AI-Powered Incident Response System"}
 
+# if script is run directly, launch FastAPI app using uvicorn
+if __name__ == "__main__":
+    import uvicorn
+    # run app on localhost at port 8000 with auto-reloading enabled
+    uvicorn.run(app, host ="0.0.0.0", port = 8000, reload = True)
